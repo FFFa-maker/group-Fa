faaef2f521539fe8802e3d6758c355fec89254fa
FFFa-maker <FFFa-maker@outlook.com>
2023-04-21 18:02:00 +0800

    课程审核

--- xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/xuecheng/content/service/impl/CoursePublishServiceImpl.java
+++ xuecheng-plus-content/xuecheng-plus-content-service/src/main/java/com/xuecheng/content/service/impl/CoursePublishServiceImpl.java
@@ -1,16 +1,27 @@
 package com.xuecheng.content.service.impl;
 
+import com.alibaba.fastjson.JSON;
+import com.xuecheng.base.exception.XueChengPlusException;
 import com.xuecheng.content.mapper.CourseBaseMapper;
-import com.xuecheng.content.mapper.TeachplanMapper;
+import com.xuecheng.content.mapper.CourseMarketMapper;
+import com.xuecheng.content.mapper.CoursePublishMapper;
+import com.xuecheng.content.mapper.CoursePublishPreMapper;
 import com.xuecheng.content.model.dto.CourseBaseInfoDto;
 import com.xuecheng.content.model.dto.CoursePreviewDto;
 import com.xuecheng.content.model.dto.TeachplanDto;
+import com.xuecheng.content.model.po.CourseBase;
+import com.xuecheng.content.model.po.CourseMarket;
+import com.xuecheng.content.model.po.CoursePublishPre;
 import com.xuecheng.content.service.CourseBaseInfoService;
 import com.xuecheng.content.service.CoursePublishService;
 import com.xuecheng.content.service.TeachplanService;
+import org.apache.commons.lang.StringUtils;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Service
@@ -19,6 +30,14 @@ public class CoursePublishServiceImpl implements CoursePublishService {
     CourseBaseInfoService courseBaseInfoService;
     @Autowired
     TeachplanService teachplanService;
+    @Autowired
+    CourseBaseMapper courseBaseMapper;
+    @Autowired
+    CourseMarketMapper courseMarketMapper;
+    @Autowired
+    CoursePublishMapper coursePublishMapper;
+    @Autowired
+    CoursePublishPreMapper coursePublishPreMapper;
 
     @Override
     public CoursePreviewDto getCoursePreviewInfo(Long courseId) {
@@ -28,5 +47,53 @@ public class CoursePublishServiceImpl implements CoursePublishService {
         coursePreviewDto.setCourseBase(courseBaseInfo);
         coursePreviewDto.setTeachplans(teachplanTree);
         return coursePreviewDto;
+    }
+
+    @Transactional
+    @Override
+    public void commitAudit(Long companyId, Long courseId) {
+        //约束条件验证
+        CourseBaseInfoDto courseBaseInfo = courseBaseInfoService.getCourseBaseInfo(courseId);
+        if (courseBaseInfo == null) {
+            XueChengPlusException.cast("未找到课程");
+        }
+        if(!courseBaseInfo.getCompanyId().equals(companyId)){
+            XueChengPlusException.cast("不允许提交其他机构的课程");
+        }
+        String auditStatus = courseBaseInfo.getAuditStatus();
+        if (auditStatus.equals("202003")) {
+            XueChengPlusException.cast("已提交审核无法再次提交");
+        }
+        String pic = courseBaseInfo.getPic();
+        if (StringUtils.isEmpty(pic)) {
+            XueChengPlusException.cast("请上传课程图片");
+        }
+        List<TeachplanDto> teachplanTree = teachplanService.findTeachplanTree(courseId);
+        if (teachplanTree == null || teachplanTree.size() == 0) {
+            XueChengPlusException.cast("请添加课程计划");
+        }
+        //查询基本信息、营销信息、课程计划等插入至预发布
+        CoursePublishPre coursePublishPre = new CoursePublishPre();
+        BeanUtils.copyProperties(courseBaseInfo, coursePublishPre);
+        //营销信息
+        CourseMarket courseMarket = courseMarketMapper.selectById(courseId);
+        String s = JSON.toJSONString(courseMarket);
+        coursePublishPre.setMarket(s);
+        //课程计划
+        String s1 = JSON.toJSONString(teachplanTree);
+        coursePublishPre.setTeachplan(s1);
+        coursePublishPre.setStatus("202003");
+        coursePublishPre.setCreateDate(LocalDateTime.now());
+
+        //存储或更新
+        CoursePublishPre coursePublishPreObj = coursePublishPreMapper.selectById(courseId);
+        if(coursePublishPreObj==null){
+            coursePublishPreMapper.insert(coursePublishPre);
+        }
+        coursePublishPreMapper.updateById(coursePublishPre);
+        //更新课程基本信息为已提交
+        CourseBase courseBase = courseBaseMapper.selectById(courseId);
+        courseBase.setAuditStatus("202003");
+        courseBaseMapper.updateById(courseBase);
     }
 }
